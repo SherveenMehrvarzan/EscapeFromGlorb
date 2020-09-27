b00e44e2b854be7d296273817da808e6d287cd68
SherveenMehrvarzan <71990870+SherveenMehrvarzan@users.noreply.github.com>
2020-09-27 15:46:49 -0700

    Add files via upload

---
+++ MainMenu/SceneScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneScript : MonoBehaviour
+{
+
+    public void Play()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+    }
+}
